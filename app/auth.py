a1b91f1c9f004957ca96c08764f5214789e60ac2
bydkoon <pansew@gmail.com>
2022-04-07 22:53:36 +0900

    auth init

---
+++ app/auth.py
@@ -0,0 +1,92 @@
+from typing import Optional
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
+
+from starlette.config import Config
+from starlette.requests import Request
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.responses import HTMLResponse, JSONResponse, RedirectResponse
+
+from authlib.integrations.starlette_client import OAuth
+
+
+app = FastAPI(docs_url=None, redoc_url=None)
+app.add_middleware(SessionMiddleware, secret_key='!secret')
+# Initialize our OAuth instance from the client ID and client secret specified in our .env file
+config = Config('.env')
+oauth = OAuth(config)
+
+CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+oauth.register(
+    name='google',
+    server_metadata_url=CONF_URL,
+    client_kwargs={
+        'scope': 'openid email profile'
+    }
+)
+
+
+@app.get('/login', tags=['authentication'])  # Tag it as "authentication" for our docs
+async def login(request: Request):
+    # Redirect Google OAuth back to our application
+    redirect_uri = request.url_for('auth')
+
+    return await oauth.google.authorize_redirect(request, redirect_uri)
+
+
+@app.route('/auth')
+async def auth(request: Request):
+    # Perform Google OAuth
+    token = await oauth.google.authorize_access_token(request)
+    user = await oauth.google.parse_id_token(request, token)
+
+    # Save the user
+    request.session['user'] = dict(user)
+
+    return RedirectResponse(url='/')
+
+
+@app.get('/logout', tags=['authentication'])  # Tag it as "authentication" for our docs
+async def logout(request: Request):
+    # Remove the user
+    request.session.pop('user', None)
+
+    return RedirectResponse(url='/')
+
+
+# Try to get the logged in user
+async def get_user(request: Request) -> Optional[dict]:
+    user = request.session.get('user')
+    if user is not None:
+        return user
+    else:
+        raise HTTPException(status_code=403, detail='Could not validate credentials.')
+
+    return None
+
+
+@app.route('/openapi.json')
+async def get_open_api_endpoint(request: Request, user: Optional[dict] = Depends(get_user)):  # This dependency protects our endpoint!
+    response = JSONResponse(get_openapi(title='FastAPI', version=1, routes=app.routes))
+    return response
+
+
+@app.get('/docs', tags=['documentation'])  # Tag it as "documentation" for our docs
+async def get_documentation(request: Request, user: Optional[dict] = Depends(get_user)):  # This dependency protects our endpoint!
+    response = get_swagger_ui_html(openapi_url='/openapi.json', title='Documentation')
+    return response
+
+
+@app.get('/')
+async def home(request: Request):
+    user = request.session.get('user')
+    if user is not None:
+        email = user['email']
+        html = (
+            f'<pre>Email: {email}</pre><br>'
+            '<a href="/docs">documentation</a><br>'
+            '<a href="/logout">logout</a>'
+        )
+        return HTMLResponse(html)
+    return HTMLResponse('<a href="/login">login</a>')
