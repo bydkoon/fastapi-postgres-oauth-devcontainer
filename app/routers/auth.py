5166000cf7c1c7fbbf18b667e534ae6a3eb8ea7e
bydkoon <pansew@gmail.com>
2022-04-26 20:25:08 +0900

    routers add

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,50 +1,72 @@
-import json
-from fastapi import APIRouter
-from fastapi import FastAPI
-from starlette.config import Config
-from starlette.requests import Request
-from starlette.responses import HTMLResponse, RedirectResponse
-from authlib.integrations.starlette_client import OAuth, OAuthError
+import os
+from dataclasses import asdict, dataclass
+from http import HTTPStatus
+from typing import Optional
 
-router = APIRouter()
+import requests
+from fastapi import Depends, Security
+from fastapi.security import OAuth2AuthorizationCodeBearer, SecurityScopes
 
-# Initialize our OAuth instance from the client ID and client secret specified in our .env file
-config = Config('.env')
-oauth = OAuth(config)
-
-CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
-oauth.register(
-    name='google',
-    server_metadata_url=CONF_URL,
-    client_kwargs={
-        'scope': 'openid email profile'
-    }
+AUTHORIZATION_URL = os.environ.get(
+    "AUTHORIZATION_URL", "https://accounts.google.com/o/oauth2/auth"
+)
+TOKEN_URL = os.environ.get("TOKEN_URL", "https://oauth2.googleapis.com/token")
+JWKS_URI = os.environ.get(
+    "JWKS_URI", "https://www.googleapis.com/oauth2/v3/certs"
+)
+ALGORITHMS = ["RS256"]
+USER_INFO_URL = (
+    "https://www.googleapis.com/oauth2/v2/userinfo?access_token={access_token}"
+)
+TOKEN_INFO_URL = (
+    "https://oauth2.googleapis.com/tokeninfo?access_token={access_token}"
 )
 
 
-@router.get('/login', tags=['authentication'])  # Tag it as "authentication" for our docs
-async def login(request: Request):
-    # Redirect Google OAuth back to our application
-    redirect_uri = request.url_for('auth')
-
-    return await oauth.google.authorize_redirect(request, redirect_uri)
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
 
 
-@router.route('/auth')
-async def auth(request: Request):
-    try:
-        token = await oauth.google.authorize_access_token(request)
-    except OAuthError as error:
-        return HTMLResponse(f'<h1>{error.error}</h1>')
-    user = token.get('userinfo')
-    if user:
-        request.session['user'] = dict(user)
-    return RedirectResponse(url='/')
+@dataclass
+class GoogleIDToken:
+    azp: str = ""
+    aud: str = ""
+    sub: str = ""
+    scope: str = ""
+    exp: str = ""
+    expires_in: str = ""
+    email: str = ""
+    email_verified: str = ""
+    access_type: str = ""
 
 
-@router.get('/logout', tags=['authentication'])  # Tag it as "authentication" for our docs
-async def logout(request: Request):
-    # Remove the user
-    request.session.pop('user', None)
+# Define a Authorization scheme specific to our Auth0 config
+auth0_scheme = OAuth2AuthorizationCodeBearer(
+    authorizationUrl=AUTHORIZATION_URL,
+    tokenUrl=TOKEN_URL,
+    scopes={"openid": "openid", "email": "email", "profile": "profile"},
+)
 
-    return RedirectResponse(url='/')
+
+async def get_current_user(
+    security_scopes: SecurityScopes, token: str = Security(auth0_scheme)
+):
+    if not verify(token):
+        raise AuthError(
+            error="유효하지 않은 토큰입니다", status_code=HTTPStatus.UNAUTHORIZED
+        )
+    response = requests.get(USER_INFO_URL.format(access_token=token))
+    if response.status_code == HTTPStatus.OK:
+        return response.json()
+
+
+def verify(token: str) -> bool:
+    response = requests.get(TOKEN_INFO_URL.format(access_token=token))
+    google_id_token: GoogleIDToken = (
+        GoogleIDToken(**response.json())
+        if response.status_code == HTTPStatus.OK
+        else GoogleIDToken(expires_in="0")
+    )
+    return int(google_id_token.expires_in) > 0
