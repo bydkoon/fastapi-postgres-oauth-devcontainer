66785d0bb56f54c5558244124962b73e6588063b
Dohoon Kim <pansew@gmail.com>
2022-05-17 23:12:13 +0900

    postgresql url 수정

--- app/settings.py
+++ app/settings.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     env: str = Field("prod", env="ENV")
     app_url: str = Field("http://localhost:8080", env="APP_URL")
     db_uri: str = Field(
-        "postgresql://api:example@localhost:5432/postgres", env="DB_URI"
+        "postgresql://postgres:postgres@localhost:5432/postgres", env="DB_URI"
     )
     # github_client_id: str = Field("", env="GITHUB_CLIENT_ID")
     # github_client_secret: str = Field("", env="GITHUB_CLIENT_SECRET")
