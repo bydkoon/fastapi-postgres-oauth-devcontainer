b71f1827c512954d9c64ca5df16536f7d7232cf6
Dohoon Kim <pansew@gmail.com>
2022-05-17 23:28:22 +0900

    user 관련 router 버그 수정

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,12 +1,18 @@
-from database import crud, schemas, models
 from fastapi import APIRouter, HTTPException, Depends
+from ..sql import crud, schemas, models, database
 from sqlalchemy.orm import Session
-from database.database import get_db
 
-router = APIRouter()
 
-@router.post("/users/", response_model=schemas.User)
-def create_user(user_info: schemas.UserBase, session: Session = Depends(get_db)):
+router = APIRouter(
+    prefix="/users",
+    tags=["users"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.post("/", response_model=schemas.User)
+def create_user(user_info: schemas.UserBase, session: Session = Depends(database.get_db)):
+    """db create user """
     db_user = (
         session.query(models.Users)
         .filter(models.Users.nickname == user_info.nickname)
@@ -18,29 +24,30 @@ def create_user(user_info: schemas.UserBase, session: Session = Depends(get_db))
     return crud.create_user(session, user_info)
 
 
-@router.get(path="/users/{user_id}", response_model=schemas.User)
-def get_user(user_id: int, session: Session = Depends(get_db)):
+@router.get(path="/{user_id}", response_model=schemas.User)
+def get_user(user_id: int, session: Session = Depends(database.get_db)) -> schemas.User:
+    """ get user_id on db"""
     user = session.query(models.Users).filter(models.Users.id == user_id).first()
     if user is None:
         raise HTTPException(status_code=404, detail="ID에 해당하는 User가 없습니다.")
     return user
 
 
-@app.put(path="/users/{user_id}", response_model=schemas.User)
+@router.put(path="/{user_id}", response_model=schemas.User)
 def put_user(
-    user_id: int, new_info: schemas.UserBase, session: Session = Depends(get_db)
-):
-    user_info = crud.update_user_info(session, user_id, new_info)
+    user_id: int, new_info: schemas.UserBase, session: Session = Depends(database.get_db))-> schemas.User: 
+    """ update user """
+    user_info : schemas.User = crud.update_user_info(session, user_id, new_info)
     return user_info
 
 
-@app.delete(path="/users/{user_id}")
-def delete_user(user_id: int, session: Session = Depends(get_db)):
+@router.delete(path="/{user_id}")
+def delete_user(user_id: int, session: Session = Depends(database.get_db)):
+    """ delete user """
     user_info = session.query(models.Users).get(user_id)
 
     if user_info is None:
         raise HTTPException(status_code=404, detail="ID에 해당하는 User가 없습니다.")
-
     session.delete(user_info)
     session.commit()
 
